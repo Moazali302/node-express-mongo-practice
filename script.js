877a07900e2fe0aca1b8326f33163dbe6c378d21
Moazali302 <moazj048@gmail.com>
2025-09-15 07:03:31 +0500

    Mino project

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const fs = require('fs').promises;
-const { resolve } = require('path');
-const {await}=require ("path");
- 
-                       //writeFIle 
+const fs = require("fs");
+const { resolve } = require("path");
+// const { await } = require("path");
+
+//writeFIle
 
 // fs.writeFile("index.txt", "Hey how are you ", function(err){
 //    if(err){
@@ -13,7 +13,7 @@ const {await}=require ("path");
 //    }
 // })
 
-                    //Append File 
+//Append File
 
 // fs.appendFile("index.txt","Ma thiek hu ap sunao ap kasy hain",function(err){
 //     if(err){return console.error(err);}
@@ -22,7 +22,7 @@ const {await}=require ("path");
 //     }
 // })
 
-        //Rename File 
+//Rename File
 
 // fs.rename("index.txt","Hello.txt",function(err){
 //      if(err){return console.error(err);}
@@ -30,8 +30,8 @@ const {await}=require ("path");
 //         return console.log("Rename Succesfully");
 //     }
 // })
-  
-                  //Copy File 
+
+//Copy File
 
 // fs.copyFile("Hello.txt","./copy/copy.txt",function(err){
 //     if(err){
@@ -40,8 +40,8 @@ const {await}=require ("path");
 //     else
 //         return console.log("Copy succesfully");
 // })
-     
-      //delete file
+
+//delete file
 
 // fs.unlink("./copy/copy.txt",function(err){
 //     if(err){
@@ -51,34 +51,31 @@ const {await}=require ("path");
 //         return console.log("File deleted successfully");
 //     }
 // })
-  
-     //delete folder
 
+//delete folder
 
-    //  fs.rmdir("./copy",function(err){
-    //     if(err) console.error(err.message);
-    //     else{
-    //         console.log("removed");
-    //     }
-    //  })
+//  fs.rmdir("./copy",function(err){
+//     if(err) console.error(err.message);
+//     else{
+//         console.log("removed");
+//     }
+//  })
 
-                         //Read Files
+//Read Files
 
-    //   fs.readFile("Hello.txt", "utf-8" ,function(err,Data){
-    //     if(err){
-    //         return console.error(err.message)
-    //     }
-    //     else{
-    //         return console.log(Data);
-    //     }
-    //   });
-    //   console.log("Reading File.....")
+//   fs.readFile("Hello.txt", "utf-8" ,function(err,Data){
+//     if(err){
+//         return console.error(err.message)
+//     }
+//     else{
+//         return console.log(Data);
+//     }
+//   });
+//   console.log("Reading File.....")
 
-  
-        ///Read Data using promises;
-    
+///Read Data using promises;
 
-    // const fs = require("fs").promises;
+// const fs = require("fs").promises;
 // async function readFile() {
 //   try {
 //     const data = await fs.readFile("Hello.txt", "utf8");
@@ -91,9 +88,7 @@ const {await}=require ("path");
 // readFile();
 // console.log("Reading File......")
 
-
-                       //create server
-
+//create server
 
 // const http=require("http");
 
@@ -101,26 +96,25 @@ const {await}=require ("path");
 //     res.end("Hello Muaz");
 // })
 // server.listen(3000);
- 
-        //Express.js 
 
+//Express.js
 
 const express = require("express");
 // const bodyParser = require("body-parser");
 // const cookieParser = require("cookie-parser");
 const path = require("path");
-const { name } = require('ejs');
+// const { name } = require("ejs");
 // const session = require("express-session");
 
 const app = express();
 let PORT = 4000; // default port
 
-               // Middlewares
+// Middlewares
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cookieParser());
 
-              // Session middleware
+// Session middleware
 
 // app.use(session({
 //   secret: "mySecretKey",        // secret key for session encryption
@@ -129,7 +123,7 @@ let PORT = 4000; // default port
 //   cookie: { maxAge: 60000 }     // session expiry = 1 minute
 // }));
 
-               // Routes
+// Routes
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "index.html"));
@@ -140,15 +134,15 @@ let PORT = 4000; // default port
 
 //   if (username === "Admin" && password === "123") {
 
-              // Save in cookie
+// Save in cookie
 
 //     res.cookie("username", username, { maxAge: 700000, httpOnly: true });
 
-             // Save in session
+// Save in session
 
 //     req.session.user = username;
 
-//     res.send(`Login successful 
+//     res.send(`Login successful
 //               <br><a href='/profile'>Go to Profile</a>`);
 //   } else {
 //     res.send("Invalid credentials  <br><a href='/'>Try Again</a>");
@@ -157,18 +151,18 @@ let PORT = 4000; // default port
 
 // app.get("/profile", (req, res) => {
 
-             // Check session first
+// Check session first
 
 //   if (req.session.user) {
-//     res.send(`Hello ${req.session.user}, (from SESSION) Welcome Back! 
+//     res.send(`Hello ${req.session.user}, (from SESSION) Welcome Back!
 //               <br><a href='/logout'>Logout</a>`);
-//   } 
-            // Fallback: check cookie if session expired
+//   }
+// Fallback: check cookie if session expired
 
 //   else if (req.cookies.username) {
-//     res.send(`Hello ${req.cookies.username}, (from COOKIE) Welcome Back! 
+//     res.send(`Hello ${req.cookies.username}, (from COOKIE) Welcome Back!
 //               <br><a href='/logout'>Logout</a>`);
-//   } 
+//   }
 //   else {
 //     res.send("No session/cookie found  <br><a href='/'>Login</a>");
 //   }
@@ -177,7 +171,7 @@ let PORT = 4000; // default port
 // app.get("/logout", (req, res) => {
 //   res.clearCookie("username");
 
-                 // Destroy session
+// Destroy session
 
 //   req.session.destroy(() => {
 //     res.send("Logged out  (Session + Cookie cleared) <br><a href='/'>Login again</a>");
@@ -190,65 +184,52 @@ let PORT = 4000; // default port
 //   res.status(404).send("Page not found");
 // });
 
-
-    // EJS, Dynamic Routing & Project Setup 
-
+// EJS, Dynamic Routing & Project Setup
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")));
-app.set('view engine , ejs ');
-app.set("views", path.join(__dirname, "views")); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs",{name:"Muaz"});
-})
-  const users=[
-  { id: 1, username: "muaz", name: "Muaz Jutt", followers: 1800, bio: "Backend Developer" },
-  { id: 2, username: "ali", name: "Adil Butt", followers: 200, bio: "Frontend Developer" },
-  { id: 3, username: "saif", name: "Saif Rajpoot", followers: 90, bio: "UI/UX Designer" }
-];
-  app.get("/users",(req,res)=>{
-    res.render("user.ejs",{users});
+app.set("view engine","ejs"); 
+app.set("views", path.join(__dirname, "views"));
+
+//   const users=[
+//   { id: 1, username: "muaz", name: "Muaz Jutt", followers: 1800, bio: "Backend Developer" },
+//   { id: 2, username: "ali", name: "Adil Butt", followers: 200, bio: "Frontend Developer" },
+//   { id: 3, username: "saif", name: "Saif Rajpoot", followers: 90, bio: "UI/UX Designer" }
+// ];
+//   app.get("/users",(req,res)=>{
+//     res.render("user.ejs",{users});
+//   });
+//   app.get("/profile/:username",(req,res)=>{
+//     const user=users.find(u=>u.username === req.params.username);
+//     if(user){
+//         res.render("profile.ejs",{user});
+//     }
+//     else{
+//         res.render("error.ejs",{username:req.params.username});
+//     }
+//   });
+//   //Dynamic Route
+//   app.get("/item/:name/:price",(req,res)=>{
+//     res.send(`your item is : ${req.params.name}  and quantity is : ${req.params.price
+//     }`)
+//   })
+
+app.get("/", (req, res) => {
+  fs.readdir("./files", function (err, files) {
+    res.render("index", { files: files });
   });
-  app.get("/profile/:username",(req,res)=>{
-    const user=users.find(u=>u.username === req.params.username);
-    if(user){
-        res.render("profile.ejs",{user});
-    }
-    else{
-        res.render("error.ejs",{username:req.params.username});
-    }
+});
+app.get('/file/:filename',function(req,res){
+  fs.readFile('/files/${req.params.filename}',"utf-8",function(err,filedata){
+    res.render('show')
   })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
+ app.post("/create",function(req,res){
+  fs.writeFile(`./files/${req.body.title.split(' ').join(" ")}.txt`,req.body.details,function(err){
+    res.redirect("/"); 
+  })
+ })
 
 
 
@@ -279,6 +260,3 @@ function startServer(port) {
 // Start server with auto-port finder
 
 startServer(PORT);
-
-
-
